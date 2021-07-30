4db9ab49df1383979ab02cb16453b8478b551344
VANSH DHIMAN <55615529+vanshdhiman86@users.noreply.github.com>
2021-07-31 01:04:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,91 @@
+var moves = ["Rock.png", "Paper.png", "Scissor.png" ];
+var cwins = 0;
+var uwins = 0;
+
+
+$(".btn").click(function(){
+	userMove = this.innerHTML;
+	compMove = computerMove();
+	$(".img2").attr("src","images/" + userMove + ".png");
+	switch (userMove){
+		case "Rock": switch(compMove){
+					case 0: console.log("Draw");
+							$("h1").text("Draw!");
+					break;
+					case 1: console.log("Computer wins")
+							$("h1").text("🚩 Player 1 Wins!")
+							cwins++;
+					break;
+					case 2: console.log("User wins")
+							$("h1").text("Player 2 Wins! 🚩")
+							uwins++;
+					break;
+					default: console.log()
+					}
+		break;
+
+		case "Paper": switch(compMove){
+					case 0: console.log("User wins")
+							$("h1").text("Player 2 Wins! 🚩")
+							uwins++;
+					break;
+					case 1: console.log("Draw");
+							$("h1").text("Draw!");
+					break;
+					case 2: console.log("Computer wins")
+							$("h1").text("🚩 Player 1 Wins!")
+							cwins++;
+					break;
+					default: console.log();
+					}
+		break;
+		case "Scissor": switch(compMove){
+					case 0: console.log("Computer wins")
+							$("h1").text("🚩 Player 1 Wins!")
+							cwins++;
+					break;
+					case 1: console.log("User wins")
+							$("h1").text("Player 2 Wins! 🚩")
+							uwins++;
+					break;
+
+					case 2: console.log("Draw");
+							$("h1").text("Draw!");
+					break;
+					default: console.log();
+					}
+		break;
+		default:console.log();
+	}
+
+	setTimeout(function(){
+		$(".img1").attr("src","images/rpc.gif");
+	}, 1000); 
+	checkGame();
+});
+
+
+function computerMove() {
+	var randomMove = Math.floor(Math.random() * 3);
+
+	$(".img1").attr("src","images/" + moves[randomMove]);
+
+	return randomMove;
+
+}
+
+
+function checkGame(){
+	if (uwins === 3){
+		console.log("gameend");
+		$("h1").text("You wins this round")
+		cwins = 0
+		uwins = 0
+	}
+	else if (cwins === 3){
+		console.log("gameend");
+		$("h1").text("You loose this round")
+		cwins = 0
+		uwins = 0
+	}
+}
